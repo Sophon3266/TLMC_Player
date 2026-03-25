dab47f85dfec5e2dcacb10f82402824cbe941ae8
沈哲颢 <40880198+Sophon3266@users.noreply.github.com>
2026-03-25 14:54:44 +0800

    fix icon bug;\n chore: stop tracking build artifacts and keep only useful source files

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_tlmc_player_TLMCApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.tlmc.player.TLMCApplication"
-)
-public final class _com_tlmc_player_TLMCApplication {
-}
